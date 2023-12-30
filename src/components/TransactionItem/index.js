b03c6aa2a8cfe12f6e41e1251688d5081ca09ef2
konadaprasad <151378851+konadaprasad@users.noreply.github.com>
2023-12-30 18:47:52 +0530

    Update index.js

--- src/components/TransactionItem/index.js
+++ src/components/TransactionItem/index.js
@@ -1,18 +1,25 @@
-// Write your code here
 import './index.css'
 
 const TransactionItem = props => {
-  const {listItems} = props
-  const {title, amount, type} = listItems
+  const {listItems, deleteOptions} = props
+  const {id, title, amount, type} = listItems
 
-  const deleteItem = () => {}
+  const deleteItem = () => {
+    deleteOptions(id)
+  }
+  let typeItem = ''
+  if (type === 'INCOME') {
+    typeItem = 'Income'
+  } else {
+    typeItem = 'Expenses'
+  }
   return (
     <li className="list-item">
       <p className="para4">{title}</p>
-      <p className="para4">{amount}</p>
-      <p className="para4">{type}</p>
+      <p className="para4"> RS {amount}</p>
+      <p className="para4">{typeItem}</p>
       <button
-        onClick={deleteItem()}
+        onClick={deleteItem}
         className="delete-btn"
         type="button"
         data-testid="delete"
